6cc45f483d7058f8e8de71f8a724bbde6591dba1
yesidmoli <nyamoli213@gmail.com>
2024-06-12 10:04:03 -0500

    False debug produccion

--- proyectoSEEP/settings.py
+++ proyectoSEEP/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k=%bd%u@=i619g75%-3egz&6kle01-ecj0s-80bgy@t$)a=$3g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
